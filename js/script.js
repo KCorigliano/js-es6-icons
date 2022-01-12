e187105fc35f5fe984def832a7669108a4a3c376
KCorigliano <corigliano98@hotmail.com>
2022-01-12 15:46:56 +0100

    Milestone 3

---
+++ js/script.js
@@ -0,0 +1,146 @@
+const iconArray = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+function createBoxes (array){
+    containerHTML.innerHTML='';
+    array.forEach(el => {
+    const boxHTML = document.createElement('div');
+    boxHTML.className=('box col-2 me-1 mb-4 py-2');
+    boxHTML.innerHTML= `
+        <p class="${el.family} ${el.prefix}${el.name} ${el.color}"></p>
+        <p>${el.name}</p>`;
+    containerHTML.append(boxHTML);
+    });   
+}
+
+function filteredArray (color){
+    const filtered = iconArray.filter(el => el.team === `${color}`);
+    return filtered;    
+}
+
+const containerHTML = document.querySelector('.icon');
+const selectBtn = document.getElementById('filter-icon');
+
+createBoxes (iconArray);
+
+selectBtn.addEventListener('change', (event) =>{
+    containerHTML.innerHTML='';
+    const filtered = iconArray.filter(el => el.color === `${event.target.value}`);
+    createBoxes (filtered);
+  
+    if (event.target.value==='all'){
+        createBoxes (iconArray);
+    }
+});
